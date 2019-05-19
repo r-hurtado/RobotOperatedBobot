a24da7701cf6374d10bd150e028b277dd27b545d
r-hurtado <miecatt@gmail.com>
2019-05-19 00:51:34 -0700

    Added a roll dice command, up to 100d100.

--- bot.js
+++ bot.js
@@ -1,26 +1,16 @@
-var Discord = require("discord.io")
-var logger = require("winston")
+const Discord = require("discord.js")
 var auth = require("./auth.json")
 
-// Configure logger settings
-logger.remove(logger.transports.Console)
-logger.add(new logger.transports.Console(), {
-    colorize: true
-})
-logger.level = "debug"
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-})
-bot.on("ready", function(evt) {
-    logger.info("Connected")
-    logger.info("Logged in as: ")
-    logger.info(bot.username + " - (" + bot.id + ")")
+const bot = new Discord.Client()
+bot.on("ready", () => {
+    console.log(`Logged in as ${bot.user.tag}!`)
+    bot.user.setActivity("Big titty goth girl", { type: "WATCHING" })
 })
 
+bot.login(auth.token)
+
 function magicBall(user) {
-    var stmt = Math.floor(Math.random() * 22)
+    var stmt = Math.floor(Math.random() * 50)
     var msg = ""
     switch (stmt) {
         case 0:
@@ -60,7 +50,7 @@ function magicBall(user) {
             msg = "Hi, FBI? Yes, this post right here."
             break
         case 12:
-            msg = "@" + user
+            msg = user + ", you can't ask that."
             break
         case 13:
             msg = "My sources say yes. But they also said Trump wouldn't be president, so who knows."
@@ -92,43 +82,136 @@ function magicBall(user) {
         case 22:
             msg = "Your chances are 50/50 AT BEST."
             break
+        case 23:
+            msg = "I do what I want."
+            break
+        case 24:
+            msg = "Beep-Boop. I mean, uhh, yes."
+            break
+        case 25:
+            msg = "I'm not sure I'm comfortable answering that."
+            break
+        case 26:
+            msg = "Uhhh... @miecatt#4093 wat do?"
+            break
+        case 27:
+            msg = "Sure."
+            break
+        case 28:
+            msg = "Without the hard-R, I'm sure it's fine."
+            break
+        case 29:
+            msg = "Wait, that's illegal."
+            break
+        case 30:
+            msg = "I *really* don't think you should."
+            break
+        case 31:
+            msg = "That depends, do you trust me? ||Snape kills Dumbledore.||"
+            break
+        case 32:
+            msg = "WHOA, ABSOLUTELY NOT."
+            break
+        case 33:
+            msg = "Fuck dude, IDK."
+            break
+        case 34:
+            msg = "Why are you asking that?"
+            break
+        case 35:
+            msg = "I'm not answering that."
+            break
+        case 36:
+            msg = "Ethically? Yes. Morally? No."
+            break
+        case 37:
+            msg = "Only if you think the clit is a myth."
+            break
+        case 38:
+            msg = "In a perfect world, yes."
+            break
+        case 39:
+            msg = "No."
+            break
+        case 40:
+            msg = "8=====D"
+            break
+        case 41:
+            msg = "1010. That's binary for no."
+            break
+        case 42:
+            msg = "On the record? Yes. Off the record? Emphatically yes."
+            break
+        case 43:
+            msg = "I don't think I understand that question. Could you re-phrase it?"
+            break
+        case 44:
+            msg = "(^__^)"
+            break
+        case 45:
+            msg = "<(O__O<) Kirby says no"
+            break
+        case 46:
+            msg = "Gentle persuasion will help, so yeah, go for it"
+            break
+        case 47:
+            msg = "Hitler would agree with that."
+            break
+        case 48:
+            msg = "My gut says yes."
+            break
+        case 49:
+            msg = "If you agree with the 3/5 compromise, definitely yes."
+            break
+        case 50:
+            msg = "Este mensaje es traído a usted por el robot español."
+            break
     }
     return msg
 }
 
-bot.on("message", function(user, userID, channelID, message, evt, receivedMessage) {
+function rollDice(args) {
+    var sum = 0
+    var die = 0
+    var words = args.split("d")
+    var numDice = parseInt(words[0])
+    var numSides = parseInt(words[1])
+    if (numDice > 100 || numSides > 100) return "You need to slow your roll there, champ."
+    if (numDice < 0 || numSides < 0) return "You need to speed up your roll there, bud."
+
+    var msg = ""
+    for (var i = 0; i < numDice; i++) {
+        die = Math.floor(Math.random() * (numSides - 1)) + 1
+        sum += die
+        msg += "(" + die + ") "
+    }
+
+    msg += "= " + sum.toString()
+    return msg
+}
+
+bot.on("message", function(receivedMessage) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == "!") {
-        var args = message.substring(1).split(" ")
+    if (receivedMessage.content.substring(0, 1) == "!") {
+        var args = receivedMessage.content.substring(1).split(" ")
         var cmd = args[0]
 
-        args = args.splice(1)
         switch (cmd.toLowerCase()) {
-            // !ping
             case "ping":
-                bot.sendMessage({
-                    to: channelID,
-                    message: "Pong!"
-                })
+                receivedMessage.channel.send("Pong!")
                 break
             case "pew":
-                bot.sendMessage({
-                    to: channelID,
-                    message: "PEW!"
-                })
+                receivedMessage.channel.send("PEWPEWPEW!")
                 break
             case "magic":
-                bot.sendMessage({
-                    to: channelID,
-                    message: magicBall(/*receivedMessage.author.toString()*/ "USER")
-                })
+                receivedMessage.channel.send(magicBall(receivedMessage.author.toString()))
                 break
             case "ah":
-                bot.sendMessage({
-                    to: channelID,
-                    message: "!ah"
-                })
+                receivedMessage.channel.send("This command is broken, and has since been deprecated.")
+                break
+            case "roll":
+                receivedMessage.channel.send(rollDice(args[1]))
                 break
             // Just add any case commands if you want to..
         }
